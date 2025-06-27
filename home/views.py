c5cf2134159f4d21c6bd1fdcec824d78411504f5
Samooalice <khheo98@naver.com>
2025-06-27 15:21:46 +0900

    git commit

--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 def home(request):
-    popup_message = "환영합니다! 홈페이지에 오신 것을 환영합니다."
-    return render(request, "home.html", {"popup_message": popup_message})
+    chapters = range(1, 22)  # 1~21
+    return render(request, 'home.html', {'chapters': chapters})
--- story/views.py
+++ story/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def chapter_detail(request, number):
+    return render(request, 'chapter_detail.html', {'chapter_number': number})
